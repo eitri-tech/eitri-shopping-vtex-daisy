0f9e7bec59936e2f6202e643a666d2a7ad79fe2a
Joao Porfirio <joao.porfirio@calindra.com.br>
2025-02-19 15:05:52 -0300

    fix: incluindo prop value para autocomplete da sugestao escolhida na busca

--- eitri-shopping-vtex-daisy-home/src/components/CInput/CInput.jsx
+++ eitri-shopping-vtex-daisy-home/src/components/CInput/CInput.jsx
@@ -1,21 +1,13 @@
 import {View, TextInput} from "eitri-luminus";
 export default function CInput(props) {
-  const { icon, type, backgroundColor, mask, onChange, ...rest } = props
+  const { icon, type, backgroundColor, mask, onChange, value, ...rest } = props
   return (
     <View
       className="flex items-center text-neutral-content px-2 w-full h-[40px]"
     >
-     <TextInput onChange={onChange} className="input-bordered rounded-full w-[85vw]" iconInsideLeft={
-            <svg
-              xmlns="http://www.w3.org/2000/svg"
-              viewBox="0 0 16 16"
-              fill="#000"
-              className="h-4 w-4 opacity-70"
-            >
-              <path d="M8 8a3 3 0 1 0 0-6 3 3 0 0 0 0 6ZM12.735 14c.618 0 1.093-.561.872-1.139a6.002 6.002 0 0 0-11.215 0c-.22.578.254 1.139.872 1.139h9.47Z" />
-            </svg>
-          } type={type || 'search'}  placeholder="Encontre seu produto.."/>
-          
+     <TextInput onChange={onChange} className="input-bordered rounded-full w-[85vw]" 
+      value={value}
+      type={type || 'search'}  placeholder="Encontre seu produto.."/>  
     </View>
   )
 }
