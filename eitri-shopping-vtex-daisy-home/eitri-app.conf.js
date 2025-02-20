0c9d41d6737349114715943c993fccf2d26ba5b3
Joao Porfirio <joao.porfirio@calindra.com.br>
2025-02-20 18:04:08 -0300

    chore: increase home shared version

--- eitri-shopping-vtex-daisy-home/eitri-app.conf.js
+++ eitri-shopping-vtex-daisy-home/eitri-app.conf.js
@@ -14,7 +14,7 @@ module.exports = {
   "eitri-app-dependencies": {
     "eitri-shopping-vtex-daisy-shared": {
       "isEitriAppShared": true,
-      "version": "0.3.10"
+      "version": "0.3.12"
     },
     "eitri-shopping-vtex-shared": {
       "isEitriAppShared": true,
