d5d934449a693d06d9b6891a35cfe2b0b6ea8265
mikaelsfm <mikael.schwind-terceiro@cea.com.br>
2025-03-17 15:00:55 -0300

    ajusta spacing no shared

--- eitri-shopping-vtex-daisy-shared/src/components/Spacing/Spacing.jsx
+++ eitri-shopping-vtex-daisy-shared/src/components/Spacing/Spacing.jsx
@@ -3,6 +3,6 @@ export default function Spacing(props) {
     const { height, width } = props
 
     return (
-        <View className='h-50 w-full'></View>
+        <View className={`h-${height || '50'} w-${width || 'full'}`}></View>
     )
 }
