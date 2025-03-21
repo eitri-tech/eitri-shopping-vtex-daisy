24fe7bb1da6629378320957cc58ca04c22698dfd
Joao Porfirio <joao.jporfirio-terceiro@cea.com.br>
2025-03-21 17:50:48 -0300

    refactor: ajustes v2

--- eitri-shopping-vtex-daisy-cart/src/components/Quantity/Quantity.jsx
+++ eitri-shopping-vtex-daisy-cart/src/components/Quantity/Quantity.jsx
@@ -11,11 +11,11 @@ export default function Quantity(props) {
                         <path d="M6 12L18 12" stroke="#000000" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" />
                     </svg>
                     :
-                    <Touchable onPress={() => handleItemQuantity(-1)}>
+                    <Button onClick={() => handleItemQuantity(-1)}>
                         <svg width="16px" height="16px" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
                             <path d="M6 12L18 12" stroke="#000000" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" />
                         </svg>
-                    </Touchable>
+                    </Button>
                 }
             </View>
             <View className="w-1/3 items-center justify-center pl-1">
