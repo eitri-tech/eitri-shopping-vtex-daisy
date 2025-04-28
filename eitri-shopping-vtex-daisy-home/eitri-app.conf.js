30c8958dcd9e86ec26cb292f43d8ce58143085a4
Gabriel Marinho <gabriel.marinho@calindra.com.br>
2025-04-28 15:36:06 -0300

    chore: subindo versoes

--- eitri-shopping-vtex-daisy-home/eitri-app.conf.js
+++ eitri-shopping-vtex-daisy-home/eitri-app.conf.js
@@ -5,7 +5,7 @@ module.exports = {
   "eitri-luminus": "2.0.0-alpha.7",
   "eitri-bifrost": "3.2.0",
   "eitri-commons": "2.1.2",
-  "version": "0.2.0",
+  "version": "0.2.1",
   "public-key": "4e77d855-d174-42f7-85e0-facfd9a31486",
   "applicationId": "1b07c226-a16d-4c55-b8ea-d6e3b8a87675",
   "id": "10e0ec3a-806b-48bd-97c9-bec2ab7c03ff",
