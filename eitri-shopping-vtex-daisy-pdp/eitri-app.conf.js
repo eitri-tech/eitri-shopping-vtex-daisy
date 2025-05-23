1b8cdd1080afdd9c004031ef516b7355d40059e9
Diogo Fagundes <diogo.fagundes-terceiro@cea.com.br>
2025-05-22 21:29:46 -0300

    ci: reformulando os logs da pipe

--- eitri-shopping-vtex-daisy-pdp/eitri-app.conf.js
+++ eitri-shopping-vtex-daisy-pdp/eitri-app.conf.js
@@ -5,7 +5,7 @@ module.exports = {
   "eitri-luminus": "2.0.0",
   "eitri-bifrost": "3.2.0",
   "eitri-commons": "2.1.2",
-  "version": "0.1.6",
+  "version": "0.1.7",
   "public-key": "e602eef5-c4dd-4b71-90a8-eb95c488c557",
   "applicationId": "1b07c226-a16d-4c55-b8ea-d6e3b8a87675",
   "id": "973b7288-b1bc-4f1e-bea5-0d9e77de7130",
